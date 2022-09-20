c7617d3e1f2ab8c8172b0cdfbd7ba69db53c88d5
Sakkar Chowdhury <38989084+SkrChowdhury@users.noreply.github.com>
2022-09-21 01:01:17 +0600

    country card implemented

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import './Country.css'
 
-const Country = () => {
+const Country = (props) => {
   return (
-    <div>
-      <h2>Name: </h2>
+    <div className='country'>
+      <h2>Country Name: {props.name} </h2>
+      <p>Population: {props.population}</p>
+      <p>
+        <small>Area: {props.area}</small>
+      </p>
     </div>
   );
 };
